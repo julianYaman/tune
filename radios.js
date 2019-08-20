fa78f745ed7ffe488eaa86634325bb7776a958af
Julian Yaman <julianyaman@posteo.eu>
2019-08-20 20:35:28 +0200

    Added some placeholder content into the radio list

---
+++ radios.js
@@ -0,0 +1,24 @@
+module.exports = {
+  "80s": undefined,
+  "classics": undefined,
+  "electro": undefined,
+  "jazz": undefined,
+  "iloveradio": {
+    0: {
+      "name": undefined,
+      "stream_url": undefined
+    },
+    1: {
+      "name": undefined,
+      "stream_url": undefined
+    },
+    2: {
+      "name": undefined,
+      "stream_url": undefined
+    },
+    3: {
+      "name": undefined,
+      "stream_url": undefined
+    }
+  }
+}
