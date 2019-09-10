d8eb6c7b46de285f4f155246d9d06c14b6e6b102
Julian Yaman <julianyaman@posteo.eu>
2019-09-11 01:15:13 +0200

    init-db: adding more color to the log messages

--- modules/init-database.js
+++ modules/init-database.js
@@ -4,28 +4,34 @@
  * TODO: Better looking console output for incoming errors
  * */
 const sqlite3 = require('sqlite3').verbose();
+const logger = require('./logger')
+const clc = require('cli-color')
+
+// Little fun thing here
+process.stdout.write(clc.reset);
 
 // Connecting to the database
 let db = new sqlite3.Database('tune', (err) => {
 	if (err) {
-		return console.error(err.message)
+		return console.error(clc.red(err.message))
 	}
-	console.log('Connected to the SQlite database.')
-	console.log('Creating table for the first time!')
+	console.log('[1/4] Connected to the SQlite database.')
+	console.log('[2/4] Creating table for the first time!')
 })
 
 // Creating the table playing_on
 db.run('CREATE TABLE playing_on(guild_id integer, channel text, stream_url text)', [], err => {
 	if (err) {
-		return console.error(err.message)
+		return console.error(clc.red('[3/4] ' + err.message))
 	}
-	console.log('Created successfully the new table with the name playing_on')
+	console.log(clc.green('[3/4] Created successfully the new table with the name playing_on'))
 })
+
 
 // Closing the connection after everything was done.
 db.close((err) => {
 	if (err) {
-		return console.error(err.message);
+		return console.error(clc.red('[4/4] ' + err.message));
 	}
-	console.log('Closed the connection to the database.')
+	console.log(clc.green('[4/4] Closed successfully the connection to the database.'))
 });
