42071e288325d46047b294462802f5f84449bebf
Julian Yaman <julianyaman@posteo.eu>
2019-09-11 00:54:08 +0200

    logger: better format; no json anymore

--- modules/logger.js
+++ modules/logger.js
@@ -1,10 +1,17 @@
 const { createLogger, format, transports } = require('winston');
-const { combine, timestamp, json } = format;
+const { combine, printf } = format;
+const moment = require('moment')
+
+// eslint-disable-next-line no-shadow
+const myFormat = printf(({ level, message}) => {
+	// eslint-disable-next-line no-shadow
+	const timestamp = moment().format('YYYY-MM-DD HH:mm:ss').trim();
+	return `${timestamp} - ${level}: ${message}`
+})
 
 const logger = createLogger({
 	format: combine(
-		timestamp(),
-		json()
+		myFormat
 	),
 	transports: [
 		new (transports.Console)(),
